af38433b934bf725d4e6840bb91682bd9c478262
Flora <329340098@qq.com>
2021-03-29 14:29:13 +0800

    integrate hook

--- feed-data-admin/components/changePwd/index.tsx
+++ feed-data-admin/components/changePwd/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Button, Form, Row, Col, Input, Card} from 'antd'
 
 const layout = {
-  labelCol: { span: 6 },
+  labelCol: {span:6},
   wrapperCol: { span: 18 }
 }
 
